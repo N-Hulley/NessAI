38ec038189cf8e0b4c5f07352450524bdae87138
Nick Hulley <nick.hulley98@gmail.com>
2019-11-12 17:43:42 +1100

    Basic functionallity

---
+++ source/unity/NessAI/Assets/Scripts/Caster.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caster : MonoBehaviour
+{
+    public Material SelectedMaterial;
+    Camera camera;
+    public LayerMask ChessLayer;
+    string previousHit;
+    GameObject previousObject;
+    Material previousMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, ChessLayer))
+        {
+            GameObject objectHit = hit.transform.gameObject;
+            if (objectHit.name.Equals(previousHit))
+            {
+
+            } else
+            {
+                if (previousObject != null)
+                { 
+                    previousObject.GetComponent<Renderer>().material = previousMaterial;
+                }
+                previousHit = objectHit.name;
+                previousMaterial = objectHit.GetComponent<Renderer>().material;
+                objectHit.GetComponent<Renderer>().material = SelectedMaterial;
+                previousObject = objectHit;
+            }
+
+            if (Input.GetMouseButtonDown(0)) {
+                Debug.Log(objectHit.name);
+            }
+        } else
+        {
+            if (previousObject != null)
+            {
+                previousObject.GetComponent<Renderer>().material = previousMaterial;
+                previousHit = null;
+                previousObject = null;
+                previousMaterial = null;
+            }
+        }
+    }
+}
